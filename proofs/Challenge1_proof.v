193901cd1e6bb739b4c15fdc45e4ba870cec0357
Armaël Guéneau <armael.gueneau@ens-lyon.fr>
2018-04-14 09:56:35 +0200

    Challenge1 proto

---
+++ proofs/Challenge1_proof.v
@@ -0,0 +1,21 @@
+Set Implicit Arguments.
+(* Load the CFML library. *)
+Require Import CFML.CFLib.
+(* Load the examples CF definitions. *)
+Require Import PairInsertionSort_ml.
+
+Require Import TLC.LibInt TLC.LibTactics.
+Require Import TLC.LibListZ.
+Require Import TLC.LibWf.
+Require Import Sorting Permutation.
+
+Open Scope Z_scope.
+
+Hint Constructors LocallySorted.
+Hint Constructors Permutation.
+Ltac auto_tilde ::= rew_list in *; eauto with maths.
+
+Lemma app_is_app : LibList.app = List.app.
+Admitted.
+Lemma rev_is_rev : rev = List.rev.
+Admitted.
